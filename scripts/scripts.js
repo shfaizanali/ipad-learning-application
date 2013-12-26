dd54d25358cdfd783d8a3aa8ce81f0c572489e05
Faizan Ali <sh.faizan.ali@hotmail.com>
2013-12-26 17:27:41 +0500

    answers and navigation improvements

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,12 @@
 $("div:jqmData(role='page')").on( "pagecreate", function( event ) {
     setImageWidthHeight();
     $('.footer-div').off().on( "click", tapHandler );
+    if($('.quest-mandatory').length)
+    {
+        $('.quest-mandatory').off();
+    }
+    $('.option').off().on("click",checkAnswer);
+    
 });
 
 $("div:jqmData(role='page')").on( "pageshow", function( event ) {
@@ -20,7 +26,18 @@ $("div:jqmData(role='page')").on( "pagehide", function( event )
 });
 
 
-
+function checkAnswer(event)
+{
+    if($(this).hasClass('correct'))
+    {
+        $('.quest-mandatory').off().on( "click", tapHandler );
+        alert("Correct Answer!! Please tap on '>' to go to next page");
+    }
+    else
+    {
+        alert('Wrong answer!! Please try again')
+    }
+}
 function tapHandler(event)
 {
     var href = $(this).attr('href');
