de4e9b7c6f66296fc6c941fc4bc3a0cd31937fb9
Faizan Ali <sh.faizan.ali@hotmail.com>
2014-01-03 03:56:25 +0500

    new changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,55 +1,79 @@
-$("div:jqmData(role='page')").on( "pagecreate", function( event ) {
+$("div:jqmData(role='page')").on( "pagecreate", function( event ) 
+{
     setImageWidthHeight();
-    $('.footer-div').off().on( "click", tapHandler );
-    if($('.quest-mandatory').length)
-    {
-        $('.quest-mandatory').off();
-    }
-    $('.option').off().on("click",checkAnswer);
     
+    $('.option').off().on("click",checkAnswer);
+    $('.narration-select').on('change',turnSoundOnOff);
+    if($('.narration-select').length > 0)
+    {
+        sessionStorage.narration = parseInt($('.narration-select').val());
+    }
 });
 
-$("div:jqmData(role='page')").on( "pageshow", function( event ) {
-    if($(this).children('.my_audio').length)
+$("div:jqmData(role='page')").on( "pageshow", function( event )
+{
+    var x = $(this).find('.quest-mandatory');
+    if(x.length > 0)
     {
-        $(this).children('.my_audio').get(0).play();
+       $(x).off('click').on('click', function(e) {
+            alert('Please choose the correct answer from below to Navigate to next page');
+            e.preventDefault();
+            return false;
+        });
+    }
+    
+    if(sessionStorage.narration == "1")
+    {
+        if($(this).children('.my_audio').length)
+        {
+            $(this).children('.my_audio').get(0).play();
+        }
     }
 });
 
 $("div:jqmData(role='page')").on( "pagehide", function( event ) 
 {
-    if($(this).children('.my_audio').length)
+    if(sessionStorage.narration == "1")
     {
-        $(this).children('.my_audio').get(0).pause();
-        $(this).children('.my_audio').get(0).currentTime = 0 ;
+        if($(this).children('.my_audio').length)
+        {
+            $(this).children('.my_audio').get(0).pause();
+            $(this).children('.my_audio').get(0).currentTime = 0 ;
+        }
     }
 });
 
 
+function turnSoundOnOff(event)
+{
+    sessionStorage.narration = parseInt($(this).val());
+}
+
 function checkAnswer(event)
 {
+    var selectedOption = $(this).attr('data-option');
     if($(this).hasClass('correct'))
     {
-        $('.quest-mandatory').off().on( "click", tapHandler );
-        alert("Correct Answer!! Please tap on '>' to go to next page");
+        $('.quest-mandatory').off().on('click', function(e) {
+            return true;
+        });
+        alert("You selected " +selectedOption+ " Correct Answer!! Please tap on '>' to go to next page");
     }
     else
     {
-        alert('Wrong answer!! Please try again')
+        alert("You selected " +selectedOption+ "!!. Wrong answer Please try again");
     }
-}
-function tapHandler(event)
-{
-    var href = $(this).attr('href');
-    $.mobile.navigate(href);
 }
 
 
 function setImageWidthHeight()
 {
-    var height = $(document).height();
-    var width = $(window).width();
-    $('.bg-img').height(height-50);
+    $('.img-container').height($(window).height());
+    $('.img-container').width($(window).width());
+    var height = $('.img-container').height();
+    var width = $('.img-container').width();
+    $('.bg-img').height(height);
     $('.bg-img').width(width);
+    $('.footer').css('margin-top',height-$('.footer').height());
 }
 
